272e1a4b759420e59a3fd009bca36861293aab09
Shahriar Alif <shahriarcse26@gmail.com>
2021-09-20 17:41:41 +0600

    dropsown added

--- Stephen Grider Course/widgets/src/App.js
+++ Stephen Grider Course/widgets/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+
 const items = [
   {
     title: "What is React?",
@@ -16,12 +18,38 @@ const items = [
   },
 ];
 
-function App() {
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      <Search></Search>
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
-}
+};
 export default App;
